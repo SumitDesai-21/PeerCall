1353570e9b3cc5239abd8656896fd4cf2b98205b
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-12-30 19:40:22 +0530

    Connected socket with HTTP server

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose, { mongo } from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectToSocket from "./src/controllers/socketManager.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); 
+let port = process.env.PORT || 3000;
+/*
+Create HTTP server with Express inside it
+Socket.io needs the raw HTTP server to attach WebSocket functionality */
+const server = createServer(app);
+
+/* 
+Attach Socket.io to the same server i.e shares port with Express
+This allows both HTTP requests and WebSocket connections on same port */
+const io = connectToSocket(server);
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected successfully.");
+    server.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  }
+};
+
+start();
+
