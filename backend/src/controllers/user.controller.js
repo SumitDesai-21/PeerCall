8baca781d8ce8eac26dc278383d7dcc3d76982f5
SumitDesai-21 <sumit.desai3433@gmail.com>
2025-12-31 22:08:21 +0530

    Created register controller function

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,30 @@
+import { User } from "../models/user.model";
+import httpStatus from 'http-status';
+import bcrypt, {hash} from 'bcrypt';
+
+const register = async(req, res)=>{
+    const {name, username, password} = req.body;
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if(existingUser){
+            return res.status(httpStatus.FOUND).json({message: "User already exists"});
+        }
+
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // by default salting = 10
+        const newUser = new User({
+            name: name, 
+            username: username,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        res.status(httpStatus.CREATED).json({message:"User registered"});
+
+    } catch (error) {
+        res.json({message: `Something went wrong ${error}`});
+    }
+}
