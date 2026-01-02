0fe9c54541beef1293c2ea8e27e62e13fb1ce17e
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-01-02 22:40:17 +0530

    Verify token at socket connection

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,7 +1,17 @@
 import { Server } from "socket.io"
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const connectToSocket = (server) =>{
-    const io = new Server(server);
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
+
+    // Verify token on socket connection
+    io.use(verifyToken);
+
     return io;
 }
 
