8504cab8c4666121fcb8416efc80d30d5b84cbc1
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-01-02 22:42:11 +0530

    Removed token attribute from database

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,8 +4,7 @@ import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema({
     name: {type: String, required: true},
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    token: {type: String } // Only token would be stored in local storage.     
+    password: {type: String, required: true}
 })
 
 const User = mongoose.model("User", userSchema);
