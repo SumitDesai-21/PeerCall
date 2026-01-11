49e78cd27ae9d7472efc87b5218b3b09b50d7425
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-01-11 16:38:09 +0530

    Created authentication context to prevent prop drilling

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,62 @@
+// setting up context API & creating auth flow
+
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import httpStatus from 'http-status';
+import axios from "axios";
+
+const AuthContext = createContext({});
+
+const client = axios.create({
+    baseURL: "http://localhost:8080/api/users"
+})
+export default AuthContext;
+
+export const AuthProvider = ({ children }) =>{
+    const authContext = useContext(AuthContext);
+    const [userData, setUserData] = useState(authContext);
+    const router = useNavigate();
+    
+    const handleRegister = async (name, email, password) =>{
+        try {
+            let request = await client.post('/register', {
+                name: name,
+                email: email,
+                password: password
+            })
+
+            if(request.status === httpStatus.CREATED){
+                return request.data.message;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // handle login here
+    const handleLogin = async (email, password) =>{
+        try {
+            let request = await client.post('/login', {
+                email: email,
+                password: password
+            })
+
+            if(request.status === httpStatus.OK){
+                localStorage.setItem("token", request.data.token);
+                router("/home");
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    const data = {
+        userData, setUserData, handleRegister, handleLogin
+    }
+
+    return (
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
