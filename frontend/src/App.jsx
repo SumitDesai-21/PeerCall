32b87ec2b8245a55e4897bfba59bbffd2e5d28dc
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-01-26 14:05:32 +0530

    Added /home route for home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Landing from './pages/Landing'
 import Authentication from './pages/Authentication'
 import { AuthProvider } from './contexts/AuthContext'
 import VideoMeet from './pages/VideoMeet'
+import Home from './pages/Home'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Routes>
             <Route path='/' element={<Landing />}></Route>
             <Route path='/auth' element={<Authentication />}></Route>
+            <Route path='/home' element={<Home/>}></Route>
             <Route path='/:url' element={<VideoMeet/>}></Route>
           </Routes>
         </AuthProvider>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import withAuth from '../utils/withAuth';
+
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default withAuth(Home);
