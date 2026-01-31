07637b3c2b5a8426b5bda7e9a2a39aec8a5b3e13
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-01-31 12:28:20 +0530

    removed imports that were not used

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
 import express from "express";
 import { createServer } from "node:http";
-import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectToSocket from "./src/controllers/socketManager.js";
 import userRoutes from './src/routes/users.routes.js';
 
-dotenv.config();
+dotenv.config();  
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
 let port = process.env.PORT || 8080;
 /*
@@ -23,7 +22,6 @@ const server = createServer(app);
 Attach Socket.io to the same server i.e shares port with Express
 This allows both HTTP requests and WebSocket connections on same port */
 const io = connectToSocket(server);
-
 
 // user routes 
 app.use('/api/users', userRoutes);
