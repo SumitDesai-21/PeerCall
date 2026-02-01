0c1b001292aaefe591e0a7e6ef222260818ca32b
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-02-01 15:33:20 +0530

    removed unused imports

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -3,7 +3,7 @@ import {
     Box, Button, Card, CardContent, TextField, Typography, Tab, Tabs, Alert, CircularProgress, Container, CssBaseline,
     IconButton
 } from "@mui/material";
-import { createTheme, duration, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import HomeIcon from "@mui/icons-material/Home";
 import AuthContext from "../contexts/AuthContext";
